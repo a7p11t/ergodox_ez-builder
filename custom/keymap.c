c2fdcab6f06f2ff6043a72f508d06d2a3395874c
junk <66475075+a7p11t@users.noreply.github.com>
2020-08-08 01:36:52 +0900

    [CHORE] Modify custom keymap
    Move NPAD one row to the right

--- custom/keymap.c
+++ custom/keymap.c
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Right
     /**/ XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, KC_NLCK ,
-    /**/ ________, XXXXXXXX, KC_P7   , KC_P8   , KC_P9   , KC_PSLS , XXXXXXXX,
-    /**/           XXXXXXXX, KC_P4   , KC_P5   , KC_P6   , KC_PAST , XXXXXXXX,
-    /**/ ________, XXXXXXXX, KC_P1   , KC_P2   , KC_P3   , KC_PMNS , XXXXXXXX,
-    /**/                     KC_P0   , KC_PDOT , KC_PEQL , KC_PPLS , XXXXXXXX,
+    /**/ ________, XXXXXXXX, XXXXXXXX, KC_P7   , KC_P8   , KC_P9   , KC_PSLS ,
+    /**/           XXXXXXXX, XXXXXXXX, KC_P4   , KC_P5   , KC_P6   , KC_PAST ,
+    /**/ ________, XXXXXXXX, XXXXXXXX, KC_P1   , KC_P2   , KC_P3   , KC_PMNS ,
+    /**/                     XXXXXXXX, KC_P0   , KC_PDOT , KC_PEQL , KC_PPLS ,
 
     /**/ ________, ________,
     /**/ ________,
